c0bacb08032b160c50704595ddaa111cf39f5e65
Pragati Srivastava <srivastavapragati662@gmail.com>
2022-05-27 08:48:25 +0000

    first_commit

---
+++ models/dim_date.sql
@@ -0,0 +1,18 @@
+{{ config (
+    materialized="table"
+)}}
+
+
+
+with dim_date as (
+
+    select extract (year from "Date of Purchase") as year,
+     extract (month from "Date of Purchase") as month,
+     extract (day from "Date of Purchase") as day,
+  "Date of Purchase" as date,
+  quarter("Date of Purchase")as quarter
+  from (select distinct "Date of Purchase" from pc_dbt_db.evtable.EV_BUYERS_DATA)
+  limit 50000000
+
+)
+select * from dim_date
